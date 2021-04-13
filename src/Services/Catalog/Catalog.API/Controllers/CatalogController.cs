2b59a684f1a7de3496e68408ab7f8b2ecfb72131
Colin Dembovsky <colindembovsky@gmail.com>
2021-04-13 09:53:57 -0500

    Testing change

--- src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -62,12 +62,10 @@ namespace Microsoft.eShopOnContainers.Services.Catalog.API.Controllers
 
             /* The "awesome" fix for testing Devspaces */
 
-            /*
             foreach (var pr in itemsOnPage) {
                 pr.Name = "Awesome " + pr.Name;
             }
 
-            */
 
             itemsOnPage = ChangeUriPlaceholder(itemsOnPage);
 
