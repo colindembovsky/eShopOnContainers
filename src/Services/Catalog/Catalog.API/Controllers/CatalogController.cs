70c8c091d3639b4cc04fa9ad52cbfd8778355ab7
Colin Dembovsky <colindembovsky@gmail.com>
2021-04-13 14:47:22 -0500

    Add the awesome fix
    Add `awesome` to each product item in the catalog.

--- src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -62,12 +62,9 @@ namespace Microsoft.eShopOnContainers.Services.Catalog.API.Controllers
 
             /* The "awesome" fix for testing Devspaces */
 
-            /*
             foreach (var pr in itemsOnPage) {
                 pr.Name = "Awesome " + pr.Name;
             }
-
-            */
 
             itemsOnPage = ChangeUriPlaceholder(itemsOnPage);
 
